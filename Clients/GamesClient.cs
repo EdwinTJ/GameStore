e4309dc82ee79e35f9290e6102ceb9bec0269781
Edwin Laptop <edwin16x@gmail.com>
2025-11-28 22:39:02 -0700

    - Render A List - Create Model  - The structure of the object - Create Client  - Store the data

---
+++ Clients/GamesClient.cs
@@ -0,0 +1,37 @@
+﻿using GameStore.Models;
+
+namespace GameStore.Clients;
+
+public class GamesClient
+{
+    private readonly List<GameSummary> games =
+    [
+        new GameSummary
+        {
+            Id = 1,
+            Name = "GTA 5",
+            Genre = "Fighting",
+            Price = 89.99M,
+            ReleaseDate = new DateOnly(2013, 7, 17),
+        },
+        new GameSummary
+        {
+            Id = 2,
+            Name = "Fornite",
+            Genre = "Battle Royale",
+            Price = 50.00M,
+            ReleaseDate = new DateOnly(2015, 4, 17),
+        },
+        new GameSummary
+        {
+            Id = 3,
+            Name = "Fifa",
+            Genre = "Sport",
+            Price = 79.99M,
+            ReleaseDate = new DateOnly(2010, 2, 17),
+        }
+    ];
+
+    // Goes from a list to an array
+    public GameSummary[] GetGames() => [..games];
+}
