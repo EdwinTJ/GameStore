f28f81d494462c36ed7b1acb55fdddee0a83da94
Edwin Laptop <edwin16x@gmail.com>
2025-11-29 00:17:27 -0700

    Add Dependency Injection

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using GameStore.Clients;
 using GameStore.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddSingleton<GamesClient>();
 
 var app = builder.Build();
 
